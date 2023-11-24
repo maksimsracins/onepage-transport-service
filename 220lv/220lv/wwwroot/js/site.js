84ad6daf60a9315aa3664858dbcfb8f4855591f7
maksimsracins <56410320+maksimsracins@users.noreply.github.com>
2023-11-24 10:10:04 +0200

    Add files via upload

---
+++ 220lv/220lv/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Function to update content based on selected language
+function updateContent(langData) {
+    document.querySelectorAll('[data-i18n]').forEach(element => {
+        const key = element.getAttribute('data-i18n');
+        element.textContent = langData[key];
+    });
+}
+// Function to fetch language data
+async function fetchLanguageData(lang) {
+    const response = await fetch(`language/${lang}.json`);
+    const words = await response.json();
+    console.log(words);
+    return words;
+}
+// Function to change language
+async function changeLanguage(lang) {
+    const langData = await fetchLanguageData(lang);
+    updateContent(langData);
+}
